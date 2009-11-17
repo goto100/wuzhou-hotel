1817c9f04083b3931d6ef420432dde4520a83a52
goto100@gmail.com <goto100@gmail.com@492d0c86-d1db-11de-8184-f791cb96e2ed>
2009-11-17 06:22:53 +0000

    展示js

--- trunk/web/script/gallery.js
+++ trunk/web/script/gallery.js
@@ -1,11 +1,20 @@
-function Gallery(ele) {
+function Gallery(ele, triggers) {
 	this.ele = ele;
+	this.triggers = triggers;
 	this.images = [];
 }
 Gallery.prototype = {
 	init: function() {
-		this.ele.css('background', 'url(res/gallery_1.png) no-repeat center center');
-		alert(this.ele.get(0))
+		var gallery = this;
+		this.ele.css('background', 'url(res/splash_1.png) no-repeat center center');
+		this.triggers.each(function(i, trigger){
+			trigger = $(trigger);
+			trigger.bind('click', function(e) {
+				this.addClass('selected');
+				gallery.ele.css('background', 'url(' + gallery.images[i].url + ') no-repeat center center');
+				return false;
+			});
+		})
 	},
 	addImage: function(url) {
 		this.images.push({url: url});
